b24ff57fdfc5195845fd5cee4ec2361ba26f165e
Voranto <lucasiegel9@gmail.com>
2026-03-15 17:19:02 +0100

    Implement endpoint for themoviedb
    Started the work on the controller for this API

--- src/main/java/com/example/movie/MovieTrackerApplication.java
+++ src/main/java/com/example/movie/MovieTrackerApplication.java
@@ -10,5 +10,4 @@ public class MovieTrackerApplication {
 		SpringApplication.run(MovieTrackerApplication.class, args);
 	}
 
-
 }
---
+++ src/main/java/com/example/movie/themoviedb/MovieDBController.java
@@ -0,0 +1,53 @@
+package com.example.movie.themoviedb;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+
+@RestController
+@RequestMapping("/api/moviedb")
+public class MovieDBController {
+
+    public final String API_URL = "https://api.themoviedb.org/3";
+    private final RestTemplate restTemplate = new RestTemplate();
+    @Value("${MOVIEDB_API_KEY}")
+    private String movieDbApiKey;
+
+    @GetMapping("/test")
+    public ResponseEntity<String> testEndpoint() {
+        return ResponseEntity.ok("HELLO WORLD");
+    }
+
+
+    @GetMapping("/movie/search")
+    public ResponseEntity<String> getMoviesByName(@RequestParam String name) {
+        String url = API_URL + "/search/movie" + "?query=" + name;
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(movieDbApiKey);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Map<String, String>> request = new HttpEntity<>(
+                headers
+        );
+
+        ResponseEntity<String> response = restTemplate.exchange(
+                url,
+                HttpMethod.GET,
+                request,
+                String.class
+        );
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                response.getBody()
+        );
+    }
+
+}
